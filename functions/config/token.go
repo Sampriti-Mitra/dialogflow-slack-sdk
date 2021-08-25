b316e4405ce7bff30eca691495c7f60936a3657e
Sampriti-Mitra <sampriti.mitra@razorpay.com>
2021-08-25 22:05:02 +0530

    config

---
+++ functions/config/token.go
@@ -0,0 +1,12 @@
+package config
+
+const (
+	BOT_TOKEN            = "" // required for events api callback events
+	APP_TOKEN            = "" // required for socket mode
+	CREDENTIALS_PATH     = ""
+	SLACK_SIGNING_SECRET = ""
+	// set this to false when url verification is not yet done
+	VERIFY_SECRET = true
+	PROJECT_ID = ""
+	AGENT_NAME = ""
+)
